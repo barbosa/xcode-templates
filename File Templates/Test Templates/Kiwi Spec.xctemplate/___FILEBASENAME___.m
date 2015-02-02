c3505f2f708d484d5b7f8f0d3600f36fad1b4fd0
Gustavo Barbosa <gustavocsb@gmail.com>
2015-02-02 16:27:51 -0200

    Specta and Kiwi templates.

---
+++ File Templates/Test Templates/Kiwi Spec.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,13 @@
+#import <Kiwi/Kiwi.h>
+#import "___VARIABLE_testedClass___.h"
+
+SPEC_BEGIN(___VARIABLE_testedClass___)
+
+describe(@"___VARIABLE_testedClass___", ^{
+
+    it(@"", ^{
+
+    });
+});
+
+SPEC_END
---
+++ File Templates/Test Templates/Specta Spec.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,13 @@
+#import "Specta.h"
+#import "___VARIABLE_testedClass___.h"
+
+SpecBegin(___VARIABLE_testedClass___)
+
+describe(@"___VARIABLE_testedClass___", ^{
+
+    it(@"", ^{
+
+    });
+});
+
+SpecEnd
